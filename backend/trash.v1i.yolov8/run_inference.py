be40c34be69e9e4ae77be7bd64d0598423cdf4f0
Joel Carlos <jjalipio23@gmail.com>
2025-11-10 17:35:37 -0400

    Script de treinamento e modelo de deteccao de lixeira

---
+++ backend/trash.v1i.yolov8/run_inference.py
@@ -0,0 +1,22 @@
+import torch
+from ultralytics import YOLO
+import cv2
+
+device = 0 if torch.cuda.is_available() else 'cpu'
+print(f"Usando dispositivo: {'GPU' if device == 0 else 'CPU'}")
+
+
+model_path = "../trash.v1i.yolov8/runs/train/yolov8n_1classe_gtx1650/weights/best.pt"
+
+model = YOLO(model_path)
+
+
+image_path = "ImagemTeste5.jpg"
+results = model.predict(source=image_path, device=device, imgsz=640, conf=0.5, save=True)
+
+for r in results:
+    print("Detecções:")
+    r.show()      
+    print(r.boxes)  
+
+print("\nResultados salvos em:", results[0].save_dir)
