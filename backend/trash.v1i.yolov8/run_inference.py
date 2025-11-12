f3fde42ad9cc74eb2cf433b1774520b8c6e53b2c
Joel Carlos <jjalipio23@gmail.com>
2025-11-11 21:40:42 -0400

    Refinamento do treinamento da IA para deteccao de lixo

--- backend/trash.v1i.yolov8/run_inference.py
+++ backend/trash.v1i.yolov8/run_inference.py
@@ -6,12 +6,12 @@ device = 0 if torch.cuda.is_available() else 'cpu'
 print(f"Usando dispositivo: {'GPU' if device == 0 else 'CPU'}")
 
 
-model_path = "../trash.v1i.yolov8/runs/train/yolov8n_1classe_gtx1650/weights/best.pt"
+model_path = "../trash.v1i.yolov8/runs/refined_train/weights/best.pt"
 
 model = YOLO(model_path)
 
 
-image_path = "ImagemTeste5.jpg"
+image_path = "ImagemTeste1.jpg"
 results = model.predict(source=image_path, device=device, imgsz=640, conf=0.5, save=True)
 
 for r in results:
